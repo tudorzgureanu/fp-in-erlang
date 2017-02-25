9573ba58b927a81fb24b27f1291a6370cb5e8b50
Tudor Zgureanu <tzgureanu@gmail.com>
2017-02-25 22:38:26 +0000

    add first and second modules

---
+++ first.erl
@@ -0,0 +1,29 @@
+-module(first).
+
+-export([double/1,mult/2,area/3,treble/1, square/1]).
+
+
+
+mult(X,Y) ->
+
+    X*Y.
+
+
+
+double(X) ->
+
+    mult(2,X).
+
+
+
+area(A,B,C) ->
+
+    S = (A+B+C)/2,
+
+    math:sqrt(S*(S-A)*(S-B)*(S-C)).
+
+treble(A) ->
+    3 * A.
+
+square(A) ->
+    A * A.
---
+++ second.erl
@@ -0,0 +1,11 @@
+-module(second).
+
+-import(first, [square/1]).
+
+-export([hypotenuse/2, area/2]).
+
+hypotenuse(A, B) ->
+  math:sqrt(square(A) + square(B)).
+
+area(A, B) ->
+  A * B / 2.
