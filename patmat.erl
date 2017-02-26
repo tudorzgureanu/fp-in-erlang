17c4bec61099d628c0da359d039eee89273292db
Tudor Zgureanu <tzgureanu@gmail.com>
2017-02-26 16:22:54 +0000

    add howManyEqual function

--- patmat.erl
+++ patmat.erl
@@ -1,8 +1,14 @@
 -module(patmat).
 
--export([is_zero/1]).
+-export([is_zero/1, howManyEqual/3]).
 
 is_zero(0) ->
     true;
 is_zero(_) ->
     false.
+
+howManyEqual(A, A, A) -> 3;
+howManyEqual(A, A, _) -> 2;
+howManyEqual(_, A, A) -> 2;
+howManyEqual(A, _, A) -> 2;
+howManyEqual(_, _, _) -> 0.
