ea936e99f500967966ba7502b5326e470619cd77
Tudor Zgureanu <tzgureanu@gmail.com>
2017-03-05 12:17:03 +0000

    add product function for a list (both direct and tail recursion versions)

--- lists1.erl
+++ lists1.erl
@@ -1,6 +1,6 @@
 -module(lists1).
 
--export([max_rec/1, max_tailrec/1]).
+-export([max_rec/1, max_tailrec/1, product_rec/1, product_tailrec/1]).
 
 max_rec([X]) -> X;
 max_rec([X|Xs]) -> max(X, max_rec(Xs)).
@@ -9,3 +9,11 @@ max_tailrec([X| Xs]) -> max_tailrec(Xs, X).
 
 max_tailrec([], Max) -> Max;
 max_tailrec([X| Xs], Max) -> max_tailrec(Xs, max(Max, X)).
+
+product_rec([]) -> 1;
+product_rec([X| Xs]) -> X * product_rec(Xs).
+
+product_tailrec(Xs) -> product_tailrec(Xs, 1).
+
+product_tailrec([], Prod) -> Prod;
+product_tailrec([X| Xs], Prod) -> product_tailrec(Xs, X * Prod).
